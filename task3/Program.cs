daa64b83777e13146619749201165718e7c09114
Nika <petrova1503200315@yandex.ru>
2022-08-31 21:22:15 +0300

    задача8

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число.");
+int numberA = 1;
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+while (numberA < numberB - 1)
+{   
+    if (numberA % 2 == 0)
+        {
+            Console.WriteLine(numberA = numberA + 2);
+        }
+    else 
+    {
+        Console.WriteLine(numberA = numberA + 1);
+    }
+}
