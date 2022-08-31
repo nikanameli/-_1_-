c08b0e661e8cd83bd5ce78583f94a02216ff7195
Nika <petrova1503200315@yandex.ru>
2022-08-31 20:31:47 +0300

    задача4

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 3 числа.");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB && numberA > numberC)
+    {
+        Console.WriteLine(numberA);
+    }
+else if (numberB > numberA && numberB > numberC)
+    {
+        Console.WriteLine(numberB);
+    }
+else 
+{
+    Console.WriteLine(numberC);
+}
