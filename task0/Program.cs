7de855d95d9d00056dea12573b9db98374f3ff96
Nika <petrova1503200315@yandex.ru>
2022-08-31 20:00:37 +0300

    задача2

---
+++ task0/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа.");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB) 
+{
+    Console.WriteLine(numberA +"=max");
+    Console.WriteLine(numberB +"= min");
+}
+else 
+{
+    Console.WriteLine(numberB +"=max");
+    Console.WriteLine(numberA +"=min");
+}
