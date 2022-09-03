ad4e49d274cd1c77d95da609950f6862482443fc
Nika <petrova1503200315@yandex.ru>
2022-09-03 16:08:48 +0300

    задача 15, вторая домашка

---
+++ task6/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Ввыдете порятковый номер недели от 1 до 7.");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA >7 || numberA < 1)
+{
+    Console.WriteLine("Введенный номер не соответствует условию.");
+}
+else if (numberA >1 && numberA < 6)
+{
+    Console.WriteLine($"{numberA} - будний день.");
+}
+else 
+{
+    Console.WriteLine($"{numberA} - выходной день.");
+}
