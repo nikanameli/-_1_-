e11fc5f0f3d40e7b62740d4848c6d167434d0684
Nika <petrova1503200315@yandex.ru>
2022-09-03 14:15:30 +0300

    задача 10, вторая домашка

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if (number < 100 | number > 999)
+{
+    Console.WriteLine("Число не соответствует условию.");
+}
+else 
+{
+    int digital1 = number / 100;
+    int digital2 = number / 10;
+    result = digital2 - digital1*10;
+    Console.WriteLine(result);
+}
