3f3e9a5067a36f7ae12a702f3326af45b46b880c
Nika <petrova1503200315@yandex.ru>
2022-09-03 18:21:47 +0300

    задача 13, вторая домашка

---
+++ task5/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число.");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA < 100)
+{
+    Console.WriteLine("Введено число, которое не имеет третьей цифры.");
+
+}
+else 
+{
+    while (numberA > 1000)
+    {
+        numberA = numberA / 10;
+    }
+   numberA = numberA % 10;
+}
+    
+Console.WriteLine(numberA);
+
