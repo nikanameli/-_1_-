2bf078e39f420ba2ce88b2b2312b056afe607e5f
Nika <petrova1503200315@yandex.ru>
2022-09-10 21:08:12 +0300

    Задача 27, 4 домашечка

---
+++ task11/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+int summ = 0;
+
+if (number > 0)
+{
+    while (number > 0)
+    {
+        a = number % 10;
+        summ = summ + a;
+        number = number / 10;
+       
+    }
+    Console.WriteLine(summ);
+}
+else 
+{
+    Console.WriteLine("Ваше число меньше 0!");
+}
