ad2fc519f6c375deff51f2166e94436c5cd5e1a8
Nika <petrova1503200315@yandex.ru>
2022-09-10 21:45:30 +0300

    Задача 29, 4 домашечка

---
+++ task12/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите элементы массива.");
+int [] array = new int [8];
+int i = 0;
+
+while (i < array.Length)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+Console.WriteLine(string.Join(", ", array));
