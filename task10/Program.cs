c56be63e0261564b721ca74acb6d258c6eb5c33a
Nika <petrova1503200315@yandex.ru>
2022-09-10 20:40:38 +0300

    Задача 25, 4 домашечка

---
+++ task10/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число.");
+Console.WriteLine("Введите степень.");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+int b = number;
+
+if (n > 1)
+{
+    while (a < n)
+    {
+        number = number * b;
+        a++;
+    }
+    Console.WriteLine(number);
+}
+else if (n == 1)
+{
+    Console.WriteLine("Единица в любой степени равна единице!");
+}
+else 
+{
+    Console.WriteLine("Введенное число меньше 0!");
+}
