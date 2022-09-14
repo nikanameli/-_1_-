76188f5c8c8f3ac10bae1f62c4350eb8d05ef830
Nika <petrova1503200315@yandex.ru>
2022-09-14 20:24:55 +0300

    Задача 34, дзешека 5

---
+++ task13/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] GetArray(int size, int rightRange, int leftRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+
+int N(int [] arr)
+{
+    int summ = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            summ = summ + 1;
+        }
+    }
+    return summ;
+}
+
+
+int[] array = GetArray(5, 999, 100);
+Console.WriteLine(string.Join(", ", array));
+int M = N(array);
+Console.WriteLine($"Количество четных чисел равно {M}");
+
