d4769ffda25a54aaa43a47930587c75e04385eb6
Nika <petrova1503200315@yandex.ru>
2022-09-18 22:34:22 +0300

    Задача 41, дзшечка 6, дополнение

---
+++ task19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите числа");
+string number = Console.ReadLine();
+int a = 0;
+while (number != "stop")
+{
+    int number1 = Convert.ToInt32(number);
+    if (number1 > 0)
+    {
+        a++;
+    }
+    number = Console.ReadLine();
+}
+Console.WriteLine(a);
