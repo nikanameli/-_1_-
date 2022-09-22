95e375aba25d8f7a66af770a5ae39efe0696259a
Nika <petrova1503200315@yandex.ru>
2022-09-22 22:02:07 +0300

    Задача 52, дзшечка 7, дополнение

---
+++ task21/Program.cs
@@ -0,0 +1,57 @@
+﻿int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void stolbikSumm(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {   
+        double summSt = 0;
+        for(int i = 0; i < matrix.GetLength(0); i++)
+        {
+            summSt += matrix[i,j];
+
+        }
+        double srAr = Math.Round(summSt / matrix.GetLength(0), 2);
+        Console.Write(srAr.ToString()+ " ");
+    }
+}
+//Math.Round( matrix[i,j], 1)
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+stolbikSumm(matr);
+
