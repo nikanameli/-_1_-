4f7e13a72f14e027720020204bd3b0a509680b03
Nika <petrova1503200315@yandex.ru>
2022-09-26 18:18:25 +0300

    Задача 54, дзшечка 8

---
+++ task24/Program.cs
@@ -0,0 +1,62 @@
+﻿int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void result(int[,] matrix)
+{   
+     for(int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int o_O = 0; o_O < matrix.GetLength(1); o_O++)
+        {
+            for(int j = 0; j < matrix.GetLength(1) - 1 ; j++)
+            {
+                if (matrix[i, j] > matrix[i, j + 1])
+                {
+                    int temp = matrix[i,j];
+                    matrix[i,j] = matrix[i, j + 1];
+                    matrix[i, j +1] = temp;
+                }  
+            }
+        }    
+    }
+    PrintMatrix(matrix);
+}
+
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+Console.WriteLine();
+result(matr);
