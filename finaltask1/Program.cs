77700c33b250bfb854f5f5712c66e783bcc59afa
Nika <petrova1503200315@yandex.ru>
2022-09-28 20:20:04 +0300

    задача 64, финал

---
+++ finaltask1/Program.cs
@@ -0,0 +1,21 @@
+﻿int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Nto1(int n)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+    else 
+    {   
+        Console.Write(n + " ");
+        Nto1(n - 1);
+    }
+}
+
+int number  = ReadNumber("Введите число.");
+Nto1(number);
