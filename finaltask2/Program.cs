751e2b76d5eae206af71a4d56f6d084515050f90
Nika <petrova1503200315@yandex.ru>
2022-09-29 20:23:16 +0300

    задача 66, финал

---
+++ finaltask2/Program.cs
@@ -0,0 +1,21 @@
+﻿int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int MtoN(int m, int n, int summ)
+{
+    if (n < m)
+    {
+        return summ;
+    }
+    else
+    {   
+        return MtoN(m + 1, n, summ + m);  
+    }
+}
+
+int M  = ReadNumber("Введите M.");
+int N  = ReadNumber("Введите N.");
+Console.WriteLine(MtoN(M, N, 0));
