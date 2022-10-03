a54aadba2ce175a1e788acdce0a83e3e512fe9c3
Nika <petrova1503200315@yandex.ru>
2022-10-03 21:31:51 +0300

    задача 68, финал

---
+++ finaltask3/Program.cs
@@ -0,0 +1,19 @@
+﻿int func(int m, int n) 
+{
+    if (m == 0) 
+    {
+        return n + 1;
+    }
+    else if (n == 0) 
+    {
+        return func(m - 1, 1);
+    }
+    else 
+    {
+        return func(m - 1, func(m, n - 1));
+    }
+}
+Console.WriteLine("Введите 2 числа.");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(func(m, n));
